62d38f8dc9e4ecf22dbb1e63aab49491386ff077
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-11 17:23:10 +0200

    ES FINISH

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,23 @@
+// ? creo file index.html
+// ? creo file myscript.js
+// ? inserisco il riferimento allo script creato al termine del body
+
+
+// ? dichiaro la variabile che ospiterà il prompt dell'utente (CONST)
+    // dilemma let&const per la variabile => CONST => userName
 const userName = prompt("Insert your name");
-console.log(userName);
 
+console.log("userName");
 
-document.getElementById("answer-container").innerHTML = "Your name is: " + userName;
+const userAge = prompt("Insert your userName");
+
+console.log("userName");
+
+const userColor = prompt("Insert your colorPrefer");
+
+console.log("userColor");
+
+document.getElementById("answer").innerHTML = "Your passowrd is: " + userName + userAge + userColor + 23; 
+
 
 
